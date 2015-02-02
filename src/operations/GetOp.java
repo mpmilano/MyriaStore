bc637b159e30eecffb4e7232a64a3cd247224ec7
Matthew Milano <milano@cs.cornell.edu>
2015-02-02 02:06:24 -0500

    adding in read-only predicate to GetOp.  things are really starting to work, i think

--- src/operations/GetOp.java
+++ src/operations/GetOp.java
@@ -3,8 +3,9 @@
 package operations;
 
 import remote.*;
+import access.*;
 
-public class GetOp<R_(HT), S extends Get<HBSObj>, HBSObj, H extends GetUnderlyingObj<HBSObj> & GetStore<S>> extends Operation<R_g(HT)> {
+public class GetOp<R_(HT), S extends Get<HBSObj>, HBSObj, H extends GetUnderlyingObj<HBSObj> & GetStore<S> & HasAccess<? extends Read> > extends Operation<R_g(HT)> {
 
 //public class GetOp<R_(HT), GT extends BackingStore<?,?,?,?>.RemoteObject<?>  > extends Operation<R_g(HT)> {
 
