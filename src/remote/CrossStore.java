d915e27b2cc822ddac0808d7a173a4ec429198ba
Matthew Milano <milano@cs.cornell.edu>
2015-05-07 11:37:21 -0400

    progress

--- src/remote/CrossStore.java
+++ src/remote/CrossStore.java
@@ -47,36 +47,61 @@ public class CrossStore<CausalObj extends RemoteObject, CausalType, CausalReplic
 
 	}
 
-	private class Timestamp{
-		public boolean prec(Timestamp t){
-			//TODO: what goes here?
-			//TODO: make sure it's reflexive; \le
-			return false;
+	private static class Timestamp{
+		private final Nonce n;
+		private final long time;
+		private final long epsilon;
+
+		public Timestamp(final long time, final long epsilon){
+			this.n = NonceGenerator.get();
+			this.time = time;
+			this.epsilon = epsilon;
+		}
+		
+		private boolean prec(Timestamp t){
+			if (epsilon != t.epsilon)
+				throw new RuntimeException("incomparable timestamps");
+			if (n == t.n) return true;
+			if (Math.abs(time - t.time) < epsilon) return false;
+			return (time < t.time);
+		}
+		public static boolean prec(Timestamp before, Timestamp after){
+			if (before == null && after == null) return true;
+			else if (after == null) return false;
+			else if (before == null) return true;
+			else return before.prec(after);
 		}
 
-		public boolean compare_valid(Timestamp t){
-			//TODO: what goes here?
-			//TODO: make sure it's reflexive; \le
-			return false;
+		private Timestamp mergeOf(Timestamp update){
+			return new Timestamp((time + update.time) / 2, epsilon);
+		}
+		
+		public static Timestamp update(Timestamp old, Timestamp update){
+			if (prec(old,update)) return update;
+			else if (prec(update,old)) return old;
+			else return old.mergeOf(update);
 		}
 	}
 
-	private class Ends extends TreeSet<Timestamp>{
+	private class Ends extends HashMap<CausalReplica, Timestamp>{
 		public boolean prec(Ends e){
-			Set<Timestamp> eup = e;
-			Set<Timestamp> thisup = this;
+
+			Set<CausalReplica> crs = new TreeSet<>();
+			crs.addAll(keySet());
+			crs.addAll(e.keySet());
 			
-			for (Timestamp t1: thisup){
-				for (Timestamp t2 : eup){
-					if (t1.compare_valid(t2) && (!t1.prec(t2)))
-						return false;
-				}
+			for (CausalReplica cr : crs){
+				Timestamp this_ts = get(cr);
+				Timestamp e_ts = e.get(cr);
+				if (!Timestamp.prec(this_ts,e_ts)) return false;
 			}
 			return true;
 		}
 		public Ends fast_forward(Ends future){
-			//TODO: ends needs some thinking about.
-			return null;
+			for (CausalReplica cr : future.keySet()){
+				put(cr,Timestamp.update(get(cr), future.get(cr)));
+			}
+			return this;
 		}
 	}
 
@@ -171,7 +196,20 @@ public class CrossStore<CausalObj extends RemoteObject, CausalType, CausalReplic
 		c.registerOnTick(new Runnable(){
 				@Override
 				public void run(){
-					
+					Object found = null;
+					final List<ReadSetPair> readset_new = new LinkedList<>();
+					for (ReadSetPair rsp : readset){
+						try {
+							found = c.existingObject(new Tombstone(rsp.operation_nonce).name).get();
+						}
+						catch(Exception e) { /* handled in finally */ }
+						finally {
+							if (found == null) readset_new.add(rsp);
+							else found = null;
+						}
+					}
+					readset.clear();
+					readset.addAll(readset_new);
 				}
 			});
 	}
---
+++ src/util/Pair.java
@@ -0,0 +1,9 @@
+package util;
+
+class Pair<T,V> {
+	public final T first;
+	public final V second;
+	public Pair(final T t, final V v){
+		first = t; second = v;
+	}
+}
