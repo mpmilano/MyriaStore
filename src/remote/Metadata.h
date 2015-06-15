db0eccddc95c939cf8e81c57313641173f8d00ba
Matthew Milano <milano@cs.cornell.edu>
2015-06-15 17:38:36 -0400

    hm..

--- src/remote/Metadata.h
+++ src/remote/Metadata.h
@@ -1,5 +1,10 @@
-	#define MetaData FourTuple<TreeSet<Pair<ReplicaID,Nonce>>, \
-	Nonce, ReplicaID, Ends<CReplicaID>>
+private static class MetaData<CReplicaID extends CausalSafe<CReplicaID> & Comparable<CReplicaID>> extends FourTuple<TreeSet<Pair<ReplicaID,Nonce>>, Nonce, ReplicaID, Ends<CReplicaID>> {
+	public MetaData(TreeSet<Pair<ReplicaID,Nonce>> a, Nonce b, ReplicaID c, Ends<CReplicaID> d){
+		super(a,b,c,d);
+	}
+}
+
+#define MetaData MetaData<CReplicaID>
 	#define MD_readfrom(x) x.a
 	#define MD_n(x) x.b
 	#define MD_natural_replica(x) x.c
